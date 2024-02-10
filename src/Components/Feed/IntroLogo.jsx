616ebf338ec4ade5b475be7fdc9bcdbdc2b7922f
priyanshu082 <priyanshusingh216@gmail.com>
2024-02-10 10:45:38 +0530

    responsiveness of front page

--- src/Components/Feed/IntroLogo.jsx
+++ src/Components/Feed/IntroLogo.jsx
@@ -69,12 +69,12 @@ const IntroLogo = () => {
     <div 
     ref={main}
     className="h-[100vh] w-[100vw] flex justify-center">
-       <div ref={parentVideoRef} className="absolute h-[100%] w-[100%] md:mt-[-20px] " >
-        <Image ref={imageRef} src={machine}  className={`image-transition ${isVisible ? 'opacity-0' : 'opacity-100'} absolute md:mt-[0px] sm:mt-[50%] mt-[36%]`} />
+       <div ref={parentVideoRef} className="absolute h-[100%] w-[100%] md:mt-[-2vh] " >
+        <Image ref={imageRef} src={machine}  className={`image-transition ${isVisible ? 'opacity-0' : 'opacity-100'} absolute md:mt-[0%] sm:mt-[30%] mt-[36%]`} />
         {isVisible && 
         <video ref={videoRef} autoPlay loop muted src="/Comp_1.mp4" style={{ opacity: isVisible ? 1 : 0 }} className={`video-transition ${isVisible ? 'opacity-100' : 'opacity-0'} absolute`} />}
       </div>
-    <div ref={movingText} className='font-poppins gradient-text z-20 text-[9vw] flex justify-center mt-[49%] md:mt-[16%] font-extrabold text-center '>
+    <div ref={movingText} className='font-poppins gradient-text z-20 text-[9vw] flex justify-center mt-[50%] sm:mt-[45.5%] md:mt-[17%] font-extrabold text-center '>
       <p className='ml-[4vw] cursor-pointer transition-all duration-200 hover:tracking-[0.5vw] h-[20vh]'>
       <span className='text'>MTS </span><span className='text'>-</span><span className='text'>AUV </span><span className='text'>ZHCET</span>
       </p>
