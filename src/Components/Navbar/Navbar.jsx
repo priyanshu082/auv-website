525fae8b8a3e2d4eb5b31fc49168777c40461163
priyanshu082 <priyanshusingh216@gmail.com>
2024-02-11 14:14:37 +0530

    apple font is added to repo i.e SF Pro Display

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     <motion.div className="z-[9999] absolute w-full md:shadow-3xl md:shadow-[#ffffffcc]">
 
     <motion.div
-    className={` hidden md:flex justify-center items-center font-poppins font-light text-[12px] pb-[8px] pt-[13px] bg-zinc-900` } >
+    className={` hidden md:flex justify-center items-center font-apple font-light text-[13px] pb-[8px] pt-[13px] bg-zinc-900` } >
 
       <Link 
       onClick={()=>setActive("HOME")}
@@ -35,7 +35,7 @@ const Navbar = () => {
           <Link
           onClick={()=>setActive(items.name)}
            href={items.link} key={index} 
-           className={`${active===items.name ? "text-gray-200 scale-110" : "text-[#ffffffcc]"} ml-[4vw] flex items-center font-poppins hover:text-white hover:scale-105 duration-300 ease-in` }>
+           className={`${active===items.name ? "text-gray-200 scale-110" : "text-[#ffffffcc]"} ml-[4vw] flex items-center font-apple hover:text-white hover:scale-105 duration-300 ease-in` }>
             {items.name}
           </Link>
         ))}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -64,32 +64,31 @@ export default function Home() {
   }, []);
 
   return (
-    <div ref={main} className='flex flex-col bg-black overflow-x-hidden'>
+    <div ref={main} className='flex flex-col font-apple bg-black overflow-x-hidden'>
        <div className=''>
        <IntroLogo/> 
        </div>
-      <div ref={parentVideoDiv} className='flex flex-col w-[100vw] h-[100vh] '>
-       <div className='z-20 flex flex-col justify-center items-center h-[100%] w-[100%]'>
-        <div ref={textDiv} className='text-gray-200 flex flex-col ml-[60%]'>
-         <h6 className='text-[3vw] font-extrabold'>
+      <div ref={parentVideoDiv} className='z-20 flex flex-col w-[100vw] h-[100vh] items-center justify-center '>
+        <div className='text-gray-200 flex flex-col ml-[60%] font-apple'>
+         <h6 className='text-[3vw] '>
          Double Dive
           </h6>
-          <p className='pt-[0.5vh] font-medium tracking-widest text-gray-400 pr-[2vw]'>
+          <p className='pt-[0.5vh] font-medium  text-gray-400 pr-[2vw]'>
           SEA 5.0 is our latest vehicle, still under development. Double hull design meticulously crafted to enhance drag reduction and optimize underwater dynamics. Designed and fabricated in-house by our team, SEA 5.0 represents the culmination of our expertise, dedication, and passion for pushing the boundaries of underwater exploration.
           </p>
           </div>
         </div> 
-      </div> 
+     
 
       <div className='z-20 flex flex-col md:flex-row-reverse justify-center items-center w-[100vw] pl-[4vh]'>
   
         <video src='/video2.mp4' autoPlay loop muted className='h-[60vh] w-[60vw] mr-[-8vw]'/>
      
         <div ref={textDiv} className=' text-gray-200 flex flex-col  '>
-         <h6 className='text-[3vw] font-extrabold'>
+         <h6 className='text-[3vw] '>
          Freaking Fast
           </h6>
-          <p className='pt-[0.5vh] font-medium tracking-widest text-gray-400 '>
+          <p className='pt-[0.5vh] text-gray-400 '>
           Powered by Nvidia’s top of the line boards –Jetson Xavier AGX. Customized cooling for better thermals in confined design. With 128 computing cores it can handle AI tasks on the go. Be it object detection, segmentation or if you just want to  be  pesky underwater Jetson have you got covered.
           </p>
           </div>
@@ -98,10 +97,10 @@ export default function Home() {
       <div className='z-20 flex flex-row mt-[10vh] justify-center items-center w-[100vw] pl-[4vh]'>
         <video src='/video.mp4' autoPlay loop muted className='h-[60vh] w-[60vw] '/>
         <div ref={textDiv} className=' text-gray-200 flex flex-col '>
-         <h6 className='text-[3vw] font-extrabold'>
+         <h6 className='text-[3vw] '>
          Kraken
           </h6>
-          <p className='pt-[0.5vh] font-medium tracking-widest text-gray-400 pr-[2vw]'>
+          <p className='pt-[0.5vh]  text-gray-400 pr-[2vw]'>
           SEA5.0 is not your ordinary mermaid, it got 4 BlueRobotics T200 thruster each pulling in 24 amps. That is more than your house can pull. Specially crafted for underwater use, it uses  hydrocarbon bearing lubricated by water giving it a top speed of 5m/s.  No mermaid can play tag with this bad boy
           </p>
           </div>
